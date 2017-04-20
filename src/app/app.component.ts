e8618730142232a156125527a24131bd7328f3d3
Bonnie Brennan <bjcarson75@yahoo.com>
2017-04-20 14:13:51 -0500

    Add AMP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { grow } from '../shared/shared';
 
 @Component({
-  selector: 'app-root',
+  selector: 'body',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   animations: [ grow ]
